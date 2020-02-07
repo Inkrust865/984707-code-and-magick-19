c11304ec950cbccb225099da3793809f6bfa0931
Наташа <antonenko_natash@mail.ru>
2020-02-08 00:36:21 +0300

    Добавляет создание массива динамически

--- js/setup.js
+++ js/setup.js
@@ -4,6 +4,7 @@ var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'К
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var WIZARD_COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var WIZARD_EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
+var WIZARDS_COUNT = 4;
 
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
@@ -12,51 +13,27 @@ var similarWizardTemplate = document.querySelector('#similar-wizard-template')
   .content
   .querySelector('.setup-similar-item');
 
-
-var Wizards = [
-  {
-    name: '',
-    coatColor: '',
-    eyesColor: ''
-  },
-  {
-    name: '',
-    coatColor: '',
-    eyesColor: ''
-  },
-  {
-    name: '',
-    coatColor: '',
-    eyesColor: ''
-  },
-  {
-    name: '',
-    coatColor: '',
-    eyesColor: ''
-  }
-];
-
-var getRandomElement = function (array) {
+var getRandomIndex = function (array) {
   return Math.floor(Math.random() * array.length);
 };
 
-var getRandomName = function (wizardNumberName) {
-  var indexName = getRandomElement(WIZARD_NAMES);
-  var indexSurname = getRandomElement(WIZARD_SURNAMES);
-  Wizards[wizardNumberName].name = WIZARD_NAMES[indexName] + ' ' + WIZARD_SURNAMES[indexSurname];
-  return Wizards[wizardNumberName].name;
+var getRandomName = function (wizardIndex) {
+  var indexName = getRandomIndex(WIZARD_NAMES);
+  var indexSurname = getRandomIndex(WIZARD_SURNAMES);
+  Wizards[wizardIndex].name = WIZARD_NAMES[indexName] + ' ' + WIZARD_SURNAMES[indexSurname];
+  return Wizards[wizardIndex].name;
 };
 
-var getRandomCoatColor = function (wizardNumberCoat) {
-  var indexCoatColor = getRandomElement(WIZARD_COAT_COLOR);
-  Wizards[wizardNumberCoat].coatColor = WIZARD_COAT_COLOR[indexCoatColor];
-  return Wizards[wizardNumberCoat].coatColor;
+var getRandomCoatColor = function (wizardIndex) {
+  var indexCoatColor = getRandomIndex(WIZARD_COAT_COLOR);
+  Wizards[wizardIndex].coatColor = WIZARD_COAT_COLOR[indexCoatColor];
+  return Wizards[wizardIndex].coatColor;
 };
 
-var getRandomEyesColor = function (wizardNumberEyes) {
-  var indexEyesColor = getRandomElement(WIZARD_EYES_COLOR);
-  Wizards[wizardNumberEyes].eyesColor = WIZARD_EYES_COLOR[indexEyesColor];
-  return Wizards[wizardNumberEyes].eyesColor;
+var getRandomEyesColor = function (wizardIndex) {
+  var indexEyesColor = getRandomIndex(WIZARD_EYES_COLOR);
+  Wizards[wizardIndex].eyesColor = WIZARD_EYES_COLOR[indexEyesColor];
+  return Wizards[wizardIndex].eyesColor;
 };
 
 var renderWizard = function (wizard) {
@@ -70,7 +47,9 @@ var renderWizard = function (wizard) {
 };
 
 var fragment = document.createDocumentFragment();
-for (var i = 0; i < Wizards.length; i++) {
+for (var i = 0; i < WIZARDS_COUNT; i++) {
+  var Wizards = [];
+  Wizards[i] = {};
   fragment.appendChild(renderWizard(i));
 }
 similarListElement.appendChild(fragment);
